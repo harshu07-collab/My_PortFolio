7b412fa28989a029930b2e36f6c08d79f27cea12
Harsh Gupta (New) <tech.savvy.harsh@gmail.com>
2026-05-27 19:00:45 +0530

    Fix Vercel deployment base path

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/My_PortFolio/',
+    base: '/',
     plugins: [react()],
 });
